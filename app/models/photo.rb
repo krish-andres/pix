6f513f1d6933eb0684ffbb409de03a433dc5cf9a
Krish Rajagopalan <krish.andres@gmail.com>
2015-01-21 14:03:49 -0800

    Display photos on user profile and install imagemagick for picture sizing

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image
+  has_attached_file :image, :styles => { medium: "300x300>", thumb: "100x100>"}
 
   validates_attachment :image, 
     :content_type => { :content_type => ['image/jpeg', 'image/png'] }, 
