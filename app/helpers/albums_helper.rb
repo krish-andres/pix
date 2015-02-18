6e7aeed94e4466011a663f9bc1e41dd67c0e6025
Krish Rajagopalan <krish.andres@gmail.com>
2015-02-17 17:27:27 -0800

    Add album cover (first photo in each album) and default placeholder image if the album is empty

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,10 @@
 module AlbumsHelper
+
+  def image_for(album)
+    unless album.photos.empty?
+      image_tag(album.photos.first.image.url(:medium))
+    else
+      image_tag('placeholder.jpg')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,6 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   resources :users
 
-  # resources :photos, except: [:index] do
-  #   resources :comments, only: [:create, :destroy]
-  # end
-
   root 'homes#show'
 end
 
